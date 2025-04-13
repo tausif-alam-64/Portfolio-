54dfe6b7a694754ad98f61ec9840849c68a8adcc
Tausif Alam <tausifalam87654321@gmail.com>
2025-04-13 23:58:55 +0530

    adding project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/floatingNav";
 
 import Image from "next/image";
@@ -15,6 +16,7 @@ export default function Home() {
           ]}/>
           <Hero />
           <Grid />
+          <RecentProjects />
         </div>
       </main>
     </>
