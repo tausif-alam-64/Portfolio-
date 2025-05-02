60b29bd426b4840e5831c3a0bd21a4ebe0812dbc
Tausif Alam <tausifalam87654321@gmail.com>
2025-05-02 23:59:27 +0530

    adding footer section

---
+++ components/Footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/data'
+import { profile } from 'console'
+
+const Footer = () => {
+  return (
+    <footer className='w-full pt-20 pb-10 ' id='contact'>
+       <div className='w-full absolute left-0 -bottom-72 min-h-96'>
+        <img src="/footer-grid.svg" alt="grid"  className='w-full h-full opacity-50'/>
+       </div>
+       <div className='flex flex-col items-center'>
+          <h1 className='heading lg:max-w-[45vw]'>
+            Ready to take <span className='text-purple'>your </span> digital presence to the next level?
+          </h1>
+          <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today let's discuss how I can help you achive your goals.</p>
+          <a href="mailto:nextinbytausif@gmail.com">
+            <MagicButton 
+            title="let's get in touch"
+            icon={<FaLocationArrow />} position='right' />
+          </a>
+       </div>
+       <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+        <p className='md:text-base md:font-normal font-light'>Copyright 2024 Tausif</p>
+       </div>
+       <div className='flex items-center md:gap-3 gap-6'>
+         {socialMedia.map((profile) => (
+            <div key={profile.id}><img src={profile.img} alt={profile.img} width={20} height={20}/></div>
+         ))}
+       </div>
+    </footer>
+  )
+}
+
+export default Footer
