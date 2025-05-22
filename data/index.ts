20eda905a7082549f3de353f14fe1244a1278924
Tausif Alam <tausifalam87654321@gmail.com>
2025-05-22 22:43:51 +0530

    Working on work experiance section

--- data/index.ts
+++ data/index.ts
@@ -174,28 +174,28 @@ export const navItems = [
     {
       id: 1,
       title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      desc: "Enhanced the interactivity of a web platform by developing responsive UI components using React.js and integrating them with backend services.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: " AI/ML Projects (Ongoing)",
+      desc: "Currently working on Python-based AI/ML projects, focusing on data analysis, predictive modeling, and practical AI applications.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Freelance Web Dev Project",
+      desc: "Led the development of a web platform for a client—handled everything from planning to deployment.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
       title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      desc: "Built and maintained user-facing features using modern tools like React.js and Next.js, ensuring performance and responsiveness.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
